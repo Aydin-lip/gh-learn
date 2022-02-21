134737ceacc2992992c1c410f14ddd72a282d2fb
Aydin <ms1166860@gmail.com>
2022-02-21 19:48:58 +0330

    gh-page website

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const $ = document;
+function _Query(name) {
+    return $.querySelector(name)
+}
+
+// navbar dropdown animation
+const dropdownmenu = $.querySelectorAll('.animation')
+dropdownmenu.forEach(item => {
+    item.onclick = event => {
+        let nextElem = event.target.nextElementSibling
+        console.log(item.classList)
+        if (item.classList.length = 5) {
+            nextElem.style.transform = 'translateY(25px)'
+            nextElem.style.opacity = 0
+        }
+        setInterval(() => {
+            if (item.classList.length = 4) {
+                nextElem.style.transform = 'translateY(0)'
+                nextElem.style.opacity = 1
+            }
+        }, 100);
+    }
+})
+
+// tooltip footer network
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl, {
+    delay : {show: 300, hide: 100}
+  })
+})
